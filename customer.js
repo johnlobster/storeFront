ef254b05cf4db067a6e0ad42aa62da0593c6837d
John Webster <JohnLobster@comcast.net>
2019-06-01 00:46:45 -0700

    refactored customer.js to use promises

--- customer.js
+++ customer.js
@@ -13,10 +13,37 @@ const common = require("./common-functions");
 
 // functions
 
+promiseQuery = function (queryString, queryArgs) {
+    return new Promise ( (resolve, reject) => {
+        if (! queryArgs) {
+            queryArgs = [];
+        }
+        connection.query(queryString, queryArgs, (err, res) => {
+            if (err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+promiseInquire = function (prompt) {
+    return new Promise ( (resolve, reject) => {
+        inquirer.prompt(prompt).then( (answers) => {
+            resolve(answers);
+        });
+    });
+}
+
 function customerInput() {
+    // variables have to be in function scope because no longer passing through callback chain
+    let itemId = 0;
+    let itemName = "";
+    let inquirerPrompt = [];
+    let queryResult = {};
+    let quantity = 0;
     // create a list of items for customer to select 
-    var query = connection.query("SELECT id,productName,price FROM products", (err, res) => {
-        if (err) throw err;
+    let mysqlPrompt = "SELECT id,productName,price FROM products";
+    promiseQuery(mysqlPrompt,[])
+    .then( (res) => {
         // print out table of items
         console.log("\nItems for sale\n");
         const lineString = common.underline(42,5);
@@ -31,61 +58,72 @@ function customerInput() {
                 parseFloat(res[i].price).toFixed(2)));
         } 
         console.log(lineString + "\n");
+        queryResult = res;
         // prompt user to choose an item - restricts numbers through "validate"
-        inquirer.prompt([
+        inquirerPrompt = [
             {
                 type: "input",
                 name: 'itemId',
                 message: "What is the id of the item you want (1-" + res.length + ") (q to exit) ?",
-                validate: (val) => ((parseInt(val) > 0) && (parseInt(val) <= res.length)) || (val.toLowerCase() === "q") 
+                validate: (val) => ((parseInt(val) > 0) && (parseInt(val) <= res.length)) || (val.toLowerCase() === "q")
             }  
-        ]).then( (answers) => {
-            // check for quit
-            if (answers.itemId.toLowerCase() === "q") {
-                common.cleanExit(connection, "Exiting customer app");
-            } 
-            let itemName = res[answers.itemId - 1].productName;
-            let itemId = answers.itemId;
-            // didn't quit so find out how many the customer wants
-            inquirer.prompt([
-                {
-                    type: "input",
-                    name: "quantity",
-                    message: "How many do you want ?",
-                    validate: (val) => parseInt(val) > 0
-                }
-            ]).then ( (answers) => {
-                // callback with user responses
-                // check stock
-                var query = connection.query("SELECT stock, salesValue, price FROM products WHERE id=" + itemId + " LIMIT 1", (err, res) => {
-                    if (err) throw err;
-                    if ( parseInt(res[0].stock) > parseInt(answers.quantity)){
-                        let newStock = parseInt(res[0].stock) - parseInt(answers.quantity);
-                        let newSalesValue = parseFloat(res[0].salesValue) + (parseInt(answers.quantity)* parseFloat(res[0].price));
-                        // update stock information and add to salesValue
-                        var query = connection.query("UPDATE products SET stock=" + newStock + 
-                        ", salesValue=" + newSalesValue +
-                        " WHERE id=" + itemId, 
-                            (err, res) => {
-                                if (err) throw err;
-                                console.log("\nPurchase was succesful");
-                                console.log("You bought " + answers.quantity + " " + itemName);
-                                customerInput();
-                            });
-                            
-
-                    }
-                    else {
-                        console.log();
-                        console.log(chalk.red("Not enough of item " + answers.itemId + " (" + itemName +") in stock"));
-                        console.log(chalk.red("Alter quantity or try another item\n"));
-                        customerInput();
-                    }
-                // note callback "Christmas tree"
-                });
-            });
-        });
+        ];
+        return promiseInquire(inquirerPrompt)
+    })
+    .then( (answers) => {
+        // check for quit
+        if (answers.itemId.toLowerCase() === "q") {
+            common.cleanExit(connection, "Exiting customer app");
+        } 
+        // save answers before next question
+        itemName = queryResult[answers.itemId - 1].productName;
+        itemId = answers.itemId;
+        // didn't quit so find out how many the customer wants
+        inquirerPrompt = [
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many " + itemName + " do you want ?",
+                validate: (val) => parseInt(val) > 0
+            }
+        ];
+        return promiseInquire(inquirerPrompt);
+    }
+    )
+    .then( (answers) => {
+        quantity = answers.quantity;
+        mysqlPrompt = "SELECT stock, salesValue, price FROM products WHERE id=" + itemId + " LIMIT 1";
+        // check stock
+        return promiseQuery(mysqlPrompt, []);
+    })
+    .then( (res) => {
+        if ( parseInt(res[0].stock) > parseInt(quantity)){
+            let newStock = parseInt(res[0].stock) - parseInt(quantity);
+            let newSalesValue = parseFloat(res[0].salesValue) + (parseInt(quantity)* parseFloat(res[0].price));
+            // update stock information and add to salesValue
+            mysqlPrompt = "UPDATE products SET stock=" + newStock + 
+            ", salesValue=" + newSalesValue +
+            " WHERE id=" + itemId; 
+            promiseQuery(mysqlPrompt,[])
+            .then( (res) => {
+                console.log("\nPurchase was succesful");
+                console.log("You bought " + quantity + " " + itemName);
+            })
+            .catch( (err) => {
+                throw err;
+            });    
+        }
+        else {
+            console.log();
+            console.log(chalk.red("Not enough of item " + itemId + " (" + itemName +") in stock"));
+            console.log(chalk.red("Alter quantity or try another item\n"));
+        }
+    })
+    .then( () => customerInput())
+    // if any prommise in chain fails, catch it here
+    .catch( (err) => {
     });
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
